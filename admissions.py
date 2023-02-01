8df19e1d6d41a040ac5b6fd4287d447d79a046d7
wanyingtian <wanying.tian@outlook.com>
2023-01-31 16:34:14 -0800

    project initialized

---
+++ admissions.py
@@ -0,0 +1,15 @@
+import app
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+import tensorflow as tf
+from tensorflow	import keras
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.keras import layers
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import Normalizer
+from sklearn.metrics import r2_score
